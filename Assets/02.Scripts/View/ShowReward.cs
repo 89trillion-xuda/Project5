b53b1f649688b9cc3b395cae7e696762c05d429a
xuda <xuda@89trillion.com>
2021-11-30 18:30:10 +0800

    大改： 1、修改RewardComponents.cs命名为RewardChildObjects.cs 2、优化，为预制体的子对象集合类RewardChildObjects.cs定义了接口，用于接收数据，并设置每个预制体对象下的子对象组件与数据一一对应，以此来使子对象集合类中的public关键字得到替换。 3、修改题目5的增加段位时已领取的奖励还会刷新的bug 4、为题目5的个人信息面板增加背景并切图 5、更新了题目5的流程

--- Assets/02.Scripts/View/ShowReward.cs
+++ Assets/02.Scripts/View/ShowReward.cs
@@ -5,14 +5,12 @@ using UnityEngine;
 using UnityEngine.UI;
 
 /// <summary>
-/// 根据数据展示所有奖励里程碑
+/// 将数据和实例化出的每个预制体的组件进行赋值映射，根据数据展示所有奖励里程碑
 /// </summary>
 public class ShowReward : MonoBehaviour
 {
     //获得单个奖励预制体克隆
     [SerializeField] private GameObject RewardObjectClone;
-    //获得预制体内部组件
-    [SerializeField] private RewardComponents RewardComponentsClone;
     //获得按钮控制类ButtonController.cs脚本
     [SerializeField] private ButtonController ButtonController;
     
@@ -22,28 +20,9 @@ public class ShowReward : MonoBehaviour
         GetRewards getRewards = new GetRewards();
         getRewards.Awake();
         List<RewardModel> rewardModels = getRewards.rewardModels;
-        
-        //得到预制体的各个组件
-        Text RewardRankPointsTxt = RewardComponentsClone.RewardRankPointsTxt;
-        Text RewardTxt = RewardComponentsClone.RewardTxt;
-        Text RewardContentTxt = RewardComponentsClone.RewardContentTxt;
-        Text RewardReceivedTxt = RewardComponentsClone.RewardReceivedTxt;
-        Button RewardReceiveBtn = RewardComponentsClone.RewardReceiveBtn;
 
-        
         for (int i = 0; i < rewardModels.Count; i++)
         {
-            //设置段位分数
-            RewardRankPointsTxt.text = rewardModels[i].RewardRankPoints1.ToString();
-            //设置奖励文本
-            RewardTxt.text = rewardModels[i].RewardTxt1;
-            //设置奖励内容
-            RewardContentTxt.text = rewardModels[i].RewardContentTxt1;
-            //设置已领取文本状态
-            RewardReceivedTxt.gameObject.SetActive(rewardModels[i].RewardReceivedTxt1);
-            //设置可领取按钮状态
-            RewardReceiveBtn.gameObject.SetActive(rewardModels[i].RewardReceiveBtn1);
-
             //实例化一个预制体
             GameObject RewardObject = GameObject.Instantiate(RewardObjectClone, transform) as GameObject;
             //获取这个克隆物体的transform
@@ -52,22 +31,22 @@ public class ShowReward : MonoBehaviour
             rtf.SetParent(transform);
 
             //得到当前对象下的 组件集合类 中的 一个方法订阅这个委托
-            RewardComponents components = RewardObject.GetComponent<RewardComponents>();
+            RewardChildObjects components = RewardObject.GetComponent<RewardChildObjects>();
             ButtonController.EventAddUI += components.AddReward;
             ButtonController.EventRefreshUI += components.RefreshReward;
             // 将外部视图的ButtonController 传入到每一个实例化的预制体上，保持一致性
-            components.buttonController = ButtonController;
+            //components.buttonController = ButtonController;
+            
+            //调用接口，每一个预制体对象对应的数据，用于设置当前对象下的子对象与数据一一对应
+            components.SetItem(rewardModels[i].RewardRankPoints1,//设置段位分数
+                rewardModels[i].RewardTxt1,//设置奖励文本
+                rewardModels[i].RewardContentTxt1,//设置奖励内容
+                rewardModels[i].RewardReceivedTxt1,//设置已领取文本状态
+                rewardModels[i].RewardReceiveBtn1,//设置可领取按钮状态
+                ButtonController);// 将外部视图的ButtonController 传入到每一个实例化的预制体上，保持一致性
+
         }
         
-        //最后还原一下里程碑预制体，让它变回原来的样子
-        Invoke("InitReward",1);
-    }
-    
-    //还原里程碑奖励预制体方法
-    public void InitReward()
-    {
-        //RewardConponentsClone中的还原方法
-        RewardComponentsClone.Init();
     }
 
 }
